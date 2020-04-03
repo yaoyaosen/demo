fd5d04e561d622c50ecea7f1b8bc0af5da60b2ba
wong <wxzhao.wang217@gmail.com>
2020-04-03 19:27:22 +0800

    update

--- src/main/java/xyz/yyaos/demo/config/InterceptorConfig.java
+++ src/main/java/xyz/yyaos/demo/config/InterceptorConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import xyz.yyaos.demo.interceptor.AuthInterceptor;
+import xyz.yyaos.demo.auth.AuthInterceptor;
 
 import java.util.Arrays;
 import java.util.List;
---
+++ src/main/java/xyz/yyaos/demo/entity/user/Account.java
@@ -0,0 +1,16 @@
+package xyz.yyaos.demo.entity.user;
+
+/**
+ * 账户
+ */
+public class Account {
+
+	/**
+	 * id
+	 */
+	private String id;
+
+	/**
+	 *
+	 */
+}
